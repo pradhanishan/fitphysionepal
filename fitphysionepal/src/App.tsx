2be9b363c64f400290f19f65e910eebf81ec2956
Ishan Pradhan <ishanpradhan.ip@gmail.com>
2022-12-01 18:19:16 +0545

    add project folder structure

--- fitphysionepal/src/App.tsx
+++ fitphysionepal/src/App.tsx
@@ -1,7 +1,14 @@
 import classes from "./app.module.css";
+import Header from "./components/layout/Header/Header";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return <div className={classes.app}>Hello World</div>;
+  return (
+    <div className={classes.app}>
+      <Header />
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
